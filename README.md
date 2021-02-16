218ddf773786988c5ade95ac1442ecd77d102fe9
xcf-t <pascal.kosak@gmail.com>
2021-02-16 16:00:24 +0100

    Documentation and simple structural changes

--- README.md
+++ README.md
@@ -5,19 +5,49 @@
 
 ## Usage
 
-**Creating a new logger**
+##### Creating a new logger Instance
 
-```typescript
+```javascript
 import {Logger} from 'lwlog';
 
 const logger = Logger.create();
+
+logger.info("Hello world");
+// Hello World
 ```
 
-**Attaching the logger to the global object**
+##### Scoping a logger
+
+A scope is a categories for a specific logger instance that is supposed to help with readability.
+
+```javascript
+import {Logger} from 'lwlog';
+
+const logger = Logger.create("Application", "Webserver");
+
+logger.info("Hello World");
+// [Application][Webserver] Hello World
+```
+
+Logger instances can also be scoped inline:
+
+```javascript
+import {Logger} from 'lwlog';
+
+const logger = Logger.create("Application");
+
+logger.info("Hello World");
+// [Application] Hello World
+
+logger.scope("Webserver").info("Hello World");
+// [Application][Webserver] Hello World
+```
+
+##### Attaching the logger to the global object
 
 This creates the `global.logger` object. To avoid global pollution this is not done by default.
 
-```typescript
+```javascript
 import {Logger} from 'lwlog';
 
 Logger.attachGlobal();
--- src/logger.ts
+++ src/logger.ts
@@ -1,22 +1,45 @@
+import * as util from "util";
+
 export class Logger {
 
-    private static sinks = [];
-    private static levels: Array<LogLevel> = [];
+    public static Levels: Record<string, LogLevel> = {
+        INFO: {
+            name: '',
+            icon: '',
+            color: ''
+        }
+    };
+
+    private static sinks: Array<LogSink> = [];
 
     protected constructor(
         private scopes: Array<string>,
     ) {}
 
+    public static createSinks(...sinks: LogSink[]) {
+        this.sinks.push(...sinks);
+    }
+
     public static attachGlobal(logger?: Logger) {
         global['logger'] = logger || Logger.create();
     }
 
-    public static create(): Logger {
-        return new Logger([]);
+    public static create(...scopes: string[]): Logger {
+        return new Logger(scopes);
     }
 
-    public scope(scope: string): Logger {
+    public scope(...scope: string[]): Logger {
         return new Logger(this.scopes.concat(scope));
+    }
+
+    public log(level: LogLevel, content: string | object) {
+        if (typeof content === "object")
+            // TODO: Make this configurable
+            content = util.inspect(content, true, 3, true);
+
+        for (const sink of Logger.sinks)
+            // TODO: use formatter
+            sink.print(content);
     }
 
 }
